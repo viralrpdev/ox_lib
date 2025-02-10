ffa0bc5f00ecdaa57a8ed0174699ad13285bb38c
newbialywhodis <118001434+newbialywhodis@users.noreply.github.com>
2025-02-10 15:22:26 +0100

    Modyfikacja pliku ustawien zeby dzialal z viralem

--- resource/settings.lua
+++ resource/settings.lua
@@ -20,89 +20,34 @@ local function safeGetKvp(fn, key, default)
 end
 
 local settings = {
-    default_locale = GetConvar('ox:locale', 'en'),
     notification_position = safeGetKvp(GetResourceKvpString, 'notification_position', 'top-right'),
-    notification_audio = safeGetKvp(GetResourceKvpInt, 'notification_audio') == 1
+    notification_audio = safeGetKvp(GetResourceKvpInt, 'notification_audio') == 1,
+    locale = safeGetKvp(GetResourceKvpString, 'locale', 'en')
 }
 
-local userLocales = GetConvarInt('ox:userLocales', 1) == 1
-
-settings.locale = userLocales and safeGetKvp(GetResourceKvpString, 'locale') or settings.default_locale
-
-local function set(key, value)
-    if settings[key] == value then return false end
-
-    settings[key] = value
-    local valueType = type(value)
-
-    if valueType == 'nil' then
-        DeleteResourceKvp(key)
-    elseif valueType == 'string' then
-        SetResourceKvp(key, value)
-    elseif valueType == 'table' then
-        SetResourceKvp(key, json.encode(value))
-    elseif valueType == 'number' then
-        SetResourceKvpInt(key, value)
-    elseif valueType == 'boolean' then
-        SetResourceKvpInt(key, value and 1 or 0)
-    else
-        return false
-    end
-
-    return true
+function settings.getNotificationSettings()
+    return {
+        notification_audio = settings.notification_audio,
+        notification_position = settings.notification_position
+    }
 end
 
-RegisterCommand('ox_lib', function()
-    local inputSettings = {
-        {
-            type = 'checkbox',
-            label = locale('ui.settings.notification_audio'),
-            checked = settings.notification_audio,
-        },
-        {
-            type = 'select',
-            label = locale('ui.settings.notification_position'),
-            options = {
-                { label = locale('ui.position.top-right'),    value = 'top-right' },
-                { label = locale('ui.position.top'),          value = 'top' },
-                { label = locale('ui.position.top-left'),     value = 'top-left' },
-                { label = locale('ui.position.center-right'), value = 'center-right' },
-                { label = locale('ui.position.center-left'),  value = 'center-left' },
-                { label = locale('ui.position.bottom-right'), value = 'bottom-right' },
-                { label = locale('ui.position.bottom'),       value = 'bottom' },
-                { label = locale('ui.position.bottom-left'),  value = 'bottom-left' },
-            },
-            default = settings.notification_position,
-            required = true,
-            icon = 'message',
-        },
-    }
-
-    if userLocales then
-        table.insert(inputSettings,
-            {
-                type = 'select',
-                label = locale('ui.settings.locale'),
-                searchable = true,
-                description = locale('ui.settings.locale_description', settings.locale),
-                options = GlobalState['ox_lib:locales'],
-                default = settings.locale,
-                required = true,
-                icon = 'book',
-            })
+function settings.setNotificationAudio(value)
+    if type(value) == 'boolean' then
+        settings.notification_audio = value
+        SetResourceKvpInt('notification_audio', value and 1 or 0)
     end
+end
 
-    local input = lib.inputDialog(locale('settings'), inputSettings) --[[@as table?]]
+function settings.setNotificationPosition(value)
+    if type(value) == 'string' then
+        settings.notification_position = value
+        SetResourceKvp('notification_position', value)
+    end
+end
 
-    if not input then return end
-
-    ---@type boolean, string, string
-    local notification_audio, notification_position, locale = table.unpack(input)
-
-    if set('locale', locale) then lib.setLocale(locale) end
-
-    set('notification_position', notification_position)
-    set('notification_audio', notification_audio)
-end)
+exports('getNotificationSettings', settings.getNotificationSettings)
+exports('setNotificationAudio', settings.setNotificationAudio)
+exports('setNotificationPosition', settings.setNotificationPosition)
 
 return settings
